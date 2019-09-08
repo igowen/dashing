3b03fa22946f1f3558f420ff715d138bcbe408d4
Ian Gowen <ian@gowen.cc>
2019-09-08 17:08:11 -0400

    sketching out some algorithmic palette stuff

--- src/resources/color.rs
+++ src/resources/color.rs
@@ -291,6 +291,23 @@ impl From<Color> for [f32; 4] {
         ]
     }
 }
+
+impl From<Color> for (f32, f32, f32) {
+    fn from(c: Color) -> Self {
+        (
+            c.0[0] as f32 / 255.0,
+            c.0[1] as f32 / 255.0,
+            c.0[2] as f32 / 255.0,
+        )
+    }
+}
+
+impl From<(f32, f32, f32)> for Color {
+    fn from((r, g, b): (f32, f32, f32)) -> Self {
+        Color([(r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8])
+    }
+}
+
 impl From<Color> for [u8; 3] {
     fn from(c: Color) -> Self {
         c.0
@@ -397,6 +414,145 @@ impl From<[[u8; 3]; 16]> for Palette {
     }
 }
 
+/// Enum representing palette index. Using this enum instead of `usize` means we can theoretically
+/// avoid some amount of runtime bounds checking.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum PaletteIndex {
+    /// 0
+    P0,
+    /// 1
+    P1,
+    /// 2
+    P2,
+    /// 3
+    P3,
+    /// 4
+    P4,
+    /// 5
+    P5,
+    /// 6
+    P6,
+    /// 7
+    P7,
+    /// 8
+    P8,
+    /// 9
+    P9,
+    /// 10
+    P10,
+    /// 11
+    P11,
+    /// 12
+    P12,
+    /// 13
+    P13,
+    /// 14
+    P14,
+    /// 15
+    P15,
+}
+
+impl PaletteIndex {
+    /// Iterator over every PaletteIndex value in order.
+    pub fn iter() -> impl Iterator<Item = &'static PaletteIndex> {
+        static INDICES: [PaletteIndex; 16] = [
+            PaletteIndex::P0,
+            PaletteIndex::P1,
+            PaletteIndex::P2,
+            PaletteIndex::P3,
+            PaletteIndex::P4,
+            PaletteIndex::P5,
+            PaletteIndex::P6,
+            PaletteIndex::P7,
+            PaletteIndex::P8,
+            PaletteIndex::P9,
+            PaletteIndex::P10,
+            PaletteIndex::P11,
+            PaletteIndex::P12,
+            PaletteIndex::P13,
+            PaletteIndex::P14,
+            PaletteIndex::P15,
+        ];
+        INDICES.iter()
+    }
+}
+
+impl From<PaletteIndex> for u8 {
+    fn from(p: PaletteIndex) -> u8 {
+        match p {
+            PaletteIndex::P0 => 0,
+            PaletteIndex::P1 => 1,
+            PaletteIndex::P2 => 2,
+            PaletteIndex::P3 => 3,
+            PaletteIndex::P4 => 4,
+            PaletteIndex::P5 => 5,
+            PaletteIndex::P6 => 6,
+            PaletteIndex::P7 => 7,
+            PaletteIndex::P8 => 8,
+            PaletteIndex::P9 => 9,
+            PaletteIndex::P10 => 10,
+            PaletteIndex::P11 => 11,
+            PaletteIndex::P12 => 12,
+            PaletteIndex::P13 => 13,
+            PaletteIndex::P14 => 14,
+            PaletteIndex::P15 => 15,
+        }
+    }
+}
+
+impl From<PaletteIndex> for usize {
+    fn from(p: PaletteIndex) -> usize {
+        <PaletteIndex as Into<u8>>::into(p) as usize
+    }
+}
+
+impl std::ops::Index<PaletteIndex> for Palette {
+    type Output = [u8; 3];
+    fn index(&self, i: PaletteIndex) -> &Self::Output {
+        match i {
+            PaletteIndex::P0 => &self.colors[0],
+            PaletteIndex::P1 => &self.colors[1],
+            PaletteIndex::P2 => &self.colors[2],
+            PaletteIndex::P3 => &self.colors[3],
+            PaletteIndex::P4 => &self.colors[4],
+            PaletteIndex::P5 => &self.colors[5],
+            PaletteIndex::P6 => &self.colors[6],
+            PaletteIndex::P7 => &self.colors[7],
+            PaletteIndex::P8 => &self.colors[8],
+            PaletteIndex::P9 => &self.colors[9],
+            PaletteIndex::P10 => &self.colors[10],
+            PaletteIndex::P11 => &self.colors[11],
+            PaletteIndex::P12 => &self.colors[12],
+            PaletteIndex::P13 => &self.colors[13],
+            PaletteIndex::P14 => &self.colors[14],
+            PaletteIndex::P15 => &self.colors[15],
+        }
+    }
+}
+
+impl std::ops::IndexMut<PaletteIndex> for Palette {
+    fn index_mut(&mut self, i: PaletteIndex) -> &mut Self::Output {
+        match i {
+            PaletteIndex::P0 => &mut self.colors[0],
+            PaletteIndex::P1 => &mut self.colors[1],
+            PaletteIndex::P2 => &mut self.colors[2],
+            PaletteIndex::P3 => &mut self.colors[3],
+            PaletteIndex::P4 => &mut self.colors[4],
+            PaletteIndex::P5 => &mut self.colors[5],
+            PaletteIndex::P6 => &mut self.colors[6],
+            PaletteIndex::P7 => &mut self.colors[7],
+            PaletteIndex::P8 => &mut self.colors[8],
+            PaletteIndex::P9 => &mut self.colors[9],
+            PaletteIndex::P10 => &mut self.colors[10],
+            PaletteIndex::P11 => &mut self.colors[11],
+            PaletteIndex::P12 => &mut self.colors[12],
+            PaletteIndex::P13 => &mut self.colors[13],
+            PaletteIndex::P14 => &mut self.colors[14],
+            PaletteIndex::P15 => &mut self.colors[15],
+        }
+    }
+}
+
 impl std::ops::Index<usize> for Palette {
     type Output = [u8; 3];
     fn index(&self, i: usize) -> &[u8; 3] {
@@ -407,5 +563,146 @@ impl std::ops::Index<usize> for Palette {
 impl std::ops::IndexMut<usize> for Palette {
     fn index_mut(&mut self, i: usize) -> &mut [u8; 3] {
         self.colors.index_mut(i)
+    }
+}
+
+/// Procedural palette
+#[derive(Default)]
+pub struct ProceduralPalette([ColorExpression; 16]);
+
+impl ProceduralPalette {
+    /// Evaluate this palette.
+    ///
+    /// ```
+    /// # use dashing::resources::color::*;
+    /// let p = Palette::default();
+    /// assert_eq!(p, ProceduralPalette::default().eval(p));
+    /// ```
+    pub fn eval(&self, p: Palette) -> Palette {
+        fn convert((r, g, b): (f32, f32, f32)) -> [u8; 3] {
+            [(r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8]
+        }
+        Palette {
+            colors: [
+                convert(self.0[0].eval(p, PaletteIndex::P0)),
+                convert(self.0[1].eval(p, PaletteIndex::P1)),
+                convert(self.0[2].eval(p, PaletteIndex::P2)),
+                convert(self.0[3].eval(p, PaletteIndex::P3)),
+                convert(self.0[4].eval(p, PaletteIndex::P4)),
+                convert(self.0[5].eval(p, PaletteIndex::P5)),
+                convert(self.0[6].eval(p, PaletteIndex::P6)),
+                convert(self.0[7].eval(p, PaletteIndex::P7)),
+                convert(self.0[8].eval(p, PaletteIndex::P8)),
+                convert(self.0[9].eval(p, PaletteIndex::P9)),
+                convert(self.0[10].eval(p, PaletteIndex::P10)),
+                convert(self.0[11].eval(p, PaletteIndex::P11)),
+                convert(self.0[12].eval(p, PaletteIndex::P12)),
+                convert(self.0[13].eval(p, PaletteIndex::P13)),
+                convert(self.0[14].eval(p, PaletteIndex::P14)),
+                convert(self.0[15].eval(p, PaletteIndex::P15)),
+            ],
+        }
+    }
+}
+
+/// ColorExpression is a way to represent a conversion from one color to another.
+#[derive(Debug)]
+pub enum ColorExpression {
+    /// Null transform: do nothing and just copy the underlying palette at the same index.
+    Null,
+    /// Straight copy of a different palette index.
+    PaletteMap(PaletteIndex),
+    /// RGB construction
+    Rgb(
+        Box<ValueExpression>,
+        Box<ValueExpression>,
+        Box<ValueExpression>,
+    ),
+    /// Use the same value for all 3 channels.
+    Mono(Box<ValueExpression>),
+}
+
+impl ColorExpression {
+    /// Evaluate this expression, producing an `(f32, f32, f32)` tuple.
+    fn eval(&self, p: Palette, i: PaletteIndex) -> (f32, f32, f32) {
+        match self {
+            ColorExpression::Null => Color::from(p[i]).into(),
+            ColorExpression::PaletteMap(pi) => Color::from(p[*pi]).into(),
+            ColorExpression::Rgb(r, g, b) => (r.eval(p, i), g.eval(p, i), b.eval(p, i)),
+            ColorExpression::Mono(v) => {
+                let vv = v.eval(p, i);
+                (vv, vv, vv)
+            }
+        }
+    }
+}
+
+impl Default for ColorExpression {
+    fn default() -> Self {
+        ColorExpression::Null
+    }
+}
+
+/// Expression representing a single channel.
+///
+/// ```
+/// # use dashing::resources::color::*;
+/// let p = Palette::default();
+/// assert_eq!(ValueExpression::Constant(42.0).eval(p, PaletteIndex::P0), 42.0);
+/// assert_eq!(
+///     ValueExpression::Add(Box::new(ValueExpression::Constant(12.0)),
+///                          Box::new(ValueExpression::Mul(Box::new(ValueExpression::Constant(2.0)),
+///                                                        Box::new(ValueExpression::Constant(3.0)))))
+///         .eval(p, PaletteIndex::P0),
+///     18.0);
+/// assert_eq!(
+///     ValueExpression::Red(Box::new(ColorExpression::PaletteMap(PaletteIndex::P15))).eval(p, PaletteIndex::P0),
+///     1.0);
+/// assert_eq!(
+///     ValueExpression::Neg(Box::new(ValueExpression::Constant(42.0))).eval(p, PaletteIndex::P0),
+///     -42.0);
+/// ```
+#[derive(Debug)]
+pub enum ValueExpression {
+    /// Constant value.
+    Constant(f32),
+    /// Extract the red channel of a color.
+    Red(Box<ColorExpression>),
+    /// Extract the green channel of a color.
+    Green(Box<ColorExpression>),
+    /// Extract the blue channel of a color.
+    Blue(Box<ColorExpression>),
+    /// Negate a value.
+    Neg(Box<ValueExpression>),
+    /// Add two values.
+    Add(Box<ValueExpression>, Box<ValueExpression>),
+    /// Subtract two values.
+    Sub(Box<ValueExpression>, Box<ValueExpression>),
+    /// Multiply two values.
+    Mul(Box<ValueExpression>, Box<ValueExpression>),
+    /// Divide two values.
+    Div(Box<ValueExpression>, Box<ValueExpression>),
+    /// Return the minimum of the two values.
+    Min(Box<ValueExpression>, Box<ValueExpression>),
+    /// Return the maximum of the two values.
+    Max(Box<ValueExpression>, Box<ValueExpression>),
+}
+
+impl ValueExpression {
+    /// Evaluate this expression, producing an `f32` value.
+    pub fn eval(&self, p: Palette, i: PaletteIndex) -> f32 {
+        match self {
+            ValueExpression::Constant(f) => *f,
+            ValueExpression::Red(c) => c.eval(p, i).0,
+            ValueExpression::Green(c) => c.eval(p, i).1,
+            ValueExpression::Blue(c) => c.eval(p, i).2,
+            ValueExpression::Neg(v) => -v.eval(p, i),
+            ValueExpression::Add(a, b) => a.eval(p, i) + b.eval(p, i),
+            ValueExpression::Sub(a, b) => a.eval(p, i) - b.eval(p, i),
+            ValueExpression::Mul(a, b) => a.eval(p, i) * b.eval(p, i),
+            ValueExpression::Div(a, b) => a.eval(p, i) / b.eval(p, i),
+            ValueExpression::Min(a, b) => f32::min(a.eval(p, i), b.eval(p, i)),
+            ValueExpression::Max(a, b) => f32::max(a.eval(p, i), b.eval(p, i)),
+        }
     }
 }
